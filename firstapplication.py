3c1c34497adda77f1ed6e293ea9c41584d18d640
pathak-aman1234 <143244535+pathak-aman1234@users.noreply.github.com>
2023-08-26 10:58:48 +0530

    Add files via upload

---
+++ firstapplication.py
@@ -0,0 +1,9 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+class main_app(MDApp):
+    def build(self):
+        return MDLabel(text="ka ho bhaisahab sab thik ba na ravibhaiya")
+    
+if __name__== '__main__':
+    main_app().run()
+
